8f1deb5371d6a352b3bf95899464916af4a5de67
hfvaldesg <hfvaldesg@gmail.com>
2018-06-28 12:05:57 +0200

    add new function to identify error 404

--- main.py
+++ main.py
@@ -5,6 +5,22 @@ page_link ='http://www.elmostrador.cl/claves/sebastian-pinera/'
 
 pinera=[]
 
+#find where the Error 404 is located
+def page_counter(link):
+	n=520
+	while True:
+		new_link=link+"page/"+str(n)+"/"
+		page_response = requests.get(new_link, timeout=5)	
+		page_content = BeautifulSoup(page_response.content, "html.parser")
+		title=page_content.title
+		if title.text=="El Mostrador - Página no encontrada":
+			print("Error 404 in page: "+str(n))
+			return n
+		else:
+			print(n)
+			n=n+1
+	
+
 def articles_selector(link):
 	page_response = requests.get(link, timeout=5)
 	page_content = BeautifulSoup(page_response.content, "html.parser")
@@ -27,9 +43,13 @@ def pinera_scraper(articles):
 		page_articles.append(components)
 	return page_articles
 
-for i in range(1,50):
-	page_link='http://www.elmostrador.cl/claves/sebastian-pinera/page/'+str(i)+'/'
-	pinera_scraper(articles_selector(page_link))
-	for i in pinera_scraper(articles_selector(page_link)):
-		pinera.append(i)
-print(pinera)
+def iterator():
+	for i in range(1,5):
+		page_link='http://www.elmostrador.cl/claves/sebastian-pinera/page/'+str(i)+'/'
+		pinera_scraper(articles_selector(page_link))
+		for i in pinera_scraper(articles_selector(page_link)):
+			pinera.append(i)
+	print(pinera)
+
+#iterator()
+page_counter(page_link)
