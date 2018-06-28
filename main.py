d57310b1e4a3536c258f2fa89aa7a82f42b3b69f
hfvaldesg <hfvaldesg@gmail.com>
2018-06-28 15:23:14 +0200

    add function 'page_counter' to tui menu

--- main.py
+++ main.py
@@ -2,20 +2,27 @@ import scraper
 import tui
 
 def main():
-	menu = {'title' : 'El Mostrador Scraper',
+	menu = {'title' : 'El Mostrador Scraper (Test for tag: "sebastian-pinera")',
 		    'type' : 'menu',
 		    'subtitle' : 'Select the action: '}
 
 	option_1 = {'title' : 'First Iterator',
 		        'type' : 'function',
-		        'function' : 'count'}
+		        'function' : 'first_iterator'}
 
-	menu['options'] = [option_1]
+	option_2 = {'title' : 'Counter of pages',
+		        'type' : 'function',
+		        'function' : 'page_counter'}
+
+	menu['options'] = [option_1,option_2]
 
 	m = tui.CursesMenu(menu)
 	selected_action = m.display()
 	if selected_action['type']!='exitmenu':
 		if selected_action['type']=='function':
-			if selected_action['function']=='count':
+			if selected_action['function']=='first_iterator':
 				scraper.first_iterator()
+			elif selected_action['function']=='page_counter':
+				page_link ='http://www.elmostrador.cl/claves/sebastian-pinera/'
+				scraper.page_counter(page_link)
 main()
