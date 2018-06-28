6485d519ee2e4b81fd6f95ef29d101f5dcd146ec
hfvaldesg <hfvaldesg@gmail.com>
2018-06-28 12:57:31 +0200

    edit the functions and the way they are added to the dictionary

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ pinera=[]
 
 #find where the Error 404 is located
 def page_counter(link):
-	n=520
+	n=1
 	while True:
 		new_link=link+"page/"+str(n)+"/"
 		page_response = requests.get(new_link, timeout=5)	
@@ -15,41 +15,40 @@ def page_counter(link):
 		title=page_content.title
 		if title.text=="El Mostrador - Página no encontrada":
 			print("Error 404 in page: "+str(n))
-			return n
+			return n-1
 		else:
 			print(n)
 			n=n+1
 	
 
 def articles_selector(link):
+	articles=[]
 	page_response = requests.get(link, timeout=5)
 	page_content = BeautifulSoup(page_response.content, "html.parser")
-	articles = page_content.find_all('article',limit=10)
+	all_articles = page_content.find_all('article')
+	for i in all_articles:
+		if i.find(class_="fecha"):
+			articles.append(scraper(i))
 	return articles
-#takes a list for of articles
 
-def pinera_scraper(articles):
+def scraper(article):
 	page_articles=[]	
 	components={}
-	for i in articles:
-		date=i.find("p").text.split("|")[0]
-		titel=i.find('a').text
-		autor=i.find_all('p')[1].text
-		link=i.find('a').get('href')
-		components['date']=date
-		components['titel']=titel
-		components['autor']=autor
-		components['link']=link
-		page_articles.append(components)
+	date=article.find("p").text.split("|")[0]
+	title=article.find('a').text
+	autor=article.find_all('p')[1].text
+	link=article.find('a').get('href')
+	components['date']=date
+	components['title']=title
+	components['autor']=autor
+	components['link']=link
+	page_articles.append(components)
 	return page_articles
 
-def iterator():
-	for i in range(1,5):
+def first_iterator():
+	for i in range(1,2):
 		page_link='http://www.elmostrador.cl/claves/sebastian-pinera/page/'+str(i)+'/'
-		pinera_scraper(articles_selector(page_link))
-		for i in pinera_scraper(articles_selector(page_link)):
-			pinera.append(i)
-	print(pinera)
+		print(articles_selector(page_link))
 
-#iterator()
-page_counter(page_link)
+first_iterator()
+#page_counter(page_link)
