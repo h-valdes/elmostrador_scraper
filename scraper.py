599a5e578a3caa5ff96845227f97065ad035a89e
hfvaldesg <hfvaldesg@gmail.com>
2018-06-29 23:46:23 +0200

    obtained all the articles for the tag 'sebastian-pinera'

--- scraper.py
+++ scraper.py
@@ -4,14 +4,12 @@ import requests
 import json
 import os
 
-pinera=[]
-
 #find where the Error 404 is located
 def page_counter(link):
 	n=1
 	while True:
 		new_link=link+"page/"+str(n)+"/"
-		page_response = requests.get(new_link, timeout=5)	
+		page_response = requests.get(new_link, timeout=10)	
 		page_content = BeautifulSoup(page_response.content, "html.parser")
 		title=page_content.title
 		if title.text=="El Mostrador - Página no encontrada":
@@ -32,8 +30,9 @@ def articles_selector(link):
 
 def scraper(article):
 	data={}
+	date="a"
 	if os.path.isfile('data.txt'):
-		with open('data.txt') as outfile:
+		with open('data.txt', encoding='utf-8') as outfile:
 			data=json.load(outfile)
 		ids=list(map(int, data.keys()))
 		id_number=str(max(ids)+1)			
@@ -52,17 +51,13 @@ def scraper(article):
 		if data[i]['title']==title:
 			return			
 	data[id_number]=components
-	with open('data.txt','w') as outfile:
-		json.dump(data,outfile)
-
+	with open('data.txt','w', encoding='utf-8') as outfile:
+		json.dump(data,outfile,ensure_ascii=False,sort_keys=True, indent=4)
 
 def first_iterator():	
 	tag='sebastian-pinera'
-	#pages=page_counter('http://www.elmostrador.cl/claves/sebastian-pinera/')
-	for i in reversed(range(1,3)):
+	pages=page_counter('http://www.elmostrador.cl/claves/sebastian-pinera/')
+	for i in reversed(range(1,pages+1)):
 		page_link='http://www.elmostrador.cl/claves/'+tag+'/page/'+str(i)+'/'
 		articles_selector(page_link)
-
-
-
 
