2c6de63f4886ed38ce0a7dd1e32ad7b80ca5b67e
hfvaldesg <hfvaldesg@gmail.com>
2018-06-29 14:01:17 +0200

    change the TUI with argparse, a lot easier and quicker

--- main.py
+++ main.py
@@ -1,28 +1,12 @@
+import argparse
 import scraper
-import tui
 
-def main():
-	menu = {'title' : 'El Mostrador Scraper (Test for tag: "sebastian-pinera")',
-		    'type' : 'menu',
-		    'subtitle' : 'Select the action: '}
-
-	option_1 = {'title' : 'First Iterator',
-		        'type' : 'function',
-		        'function' : 'first_iterator'}
-
-	option_2 = {'title' : 'Counter of pages',
-		        'type' : 'function',
-		        'function' : 'page_counter'}
-
-	menu['options'] = [option_1,option_2]
-
-	m = tui.CursesMenu(menu)
-	selected_action = m.display()
-	if selected_action['type']!='exitmenu':
-		if selected_action['type']=='function':
-			if selected_action['function']=='first_iterator':
-				scraper.first_iterator()
-			elif selected_action['function']=='page_counter':
-				page_link ='http://www.elmostrador.cl/claves/sebastian-pinera/'
-				scraper.page_counter(page_link)
-main()
+parser=argparse.ArgumentParser()
+parser.add_argument('-i','--iterator',action="store_true")
+parser.add_argument('-c','--counter',action="store_true")
+args=parser.parse_args()
+if args.iterator:
+	scraper.first_iterator()
+if args.counter:
+	page_link ='http://www.elmostrador.cl/claves/sebastian-pinera/'
+	scraper.page_counter(page_link)
